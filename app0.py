852d4b3a8516ba8e77512d933b809d63e0166fea
taylnath <taylnath@oregonstate.edu>
2020-10-06 23:56:53 -0700

    cleaned up code in app0.py

--- app0.py
+++ app0.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, Response, redirect, render_template
-import os
+from flask_paginate import Pagination, get_page_args
 import requests
-from flask_paginate import Pagination, get_page_parameter, get_page_args
 import issues
 
 app = Flask(__name__)
@@ -19,25 +18,24 @@ def get_issues(offset=0, per_page=10):
 
 @app.route('/', methods=['POST', 'GET'])
 def main_page():
-    # r = requests.get("https://api.github.com/repos/walmartlabs/thorax/issues")
-    # issues = r.json()
-    print(issues)
-    # titles = [issue["title"] for issue in issues]
-    page = request.args.get(get_page_parameter(), type=int, default=1)
-    pagination = Pagination(page=page, per_page=10, total=len(issues), record_name="issues", css_framework="bootstrap4")
 
-    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-    total = len(issues)
+    # get the current page number, issues per page, and current offset
+    # pylint: disable=unbalanced-tuple-unpacking
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page', offset="offset")
+    print(page, " ", per_page, " ", offset)
 
+    # get a the current page's list of issues
     pagination_issues = get_issues(offset=offset, per_page=per_page)
-    pagination = Pagination(page=page, per_page=per_page, total=total, css_framework="bootstrap4")
+    pagination = Pagination(page=page, per_page=per_page, total=len(issues), css_framework="bootstrap4")
 
     return render_template("issue_browser.html", issues=pagination_issues, pagination=pagination)
 
 @app.route('/issue_detail')
 def issue_detail():
+    # get issue id passed to page by the issue detail link
     issue_id = request.args.get("id", None)
-    print(issue_id)
-    print([int(issue_id) == int(issue["id"]) for issue in issues])
+
+    # isolate the issue by looking it up by id in the list
     issue = [issue for issue in issues if int(issue["id"]) == int(issue_id)][0]
+
     return render_template("issue_detail.html", issue=issue)
